7b438dbae2c1f93b518878d01486dd36fdd630b9
ramdhanll <erdevlop@gmail.com>
2021-10-22 16:16:22 +0700

    update url mongodb

--- backend/src/config/Database.js
+++ backend/src/config/Database.js
@@ -2,10 +2,13 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 dotenv.config()
 // mKF52BvRh1vfgLGf
+
+// 'mongodb+srv://indonesia:indonesiaraya@cluster0.prj7h.mongodb.net/smp?retryWrites=true&w=majority',
+
 const Mongoose = () =>
    mongoose
       .connect(
-         'mongodb+srv://indonesia:indonesiaraya@cluster0.prj7h.mongodb.net/smp?retryWrites=true&w=majority',
+         'mongodb://indonesia:indonesiaraya@cluster0-shard-00-00.prj7h.mongodb.net:27017,cluster0-shard-00-01.prj7h.mongodb.net:27017,cluster0-shard-00-02.prj7h.mongodb.net:27017/smp?ssl=true&replicaSet=atlas-13k24f-shard-0&authSource=admin&retryWrites=true&w=majority',
          {
             useUnifiedTopology: true,
             useNewUrlParser: true,
